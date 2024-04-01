fa53585282fc117a69769fae593147f176c756f6
Jaynayon <jay.nayonjr@cit.edu>
2024-04-01 11:29:24 +0800

    Remove ID Property

--- princi-pal/models/school.js
+++ princi-pal/models/school.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose')
 
 const schoolSchema = mongoose.Schema({
-    id: {
-        type: number,
-        required: true
-    },
     name: {
         type: String,
         required: true
