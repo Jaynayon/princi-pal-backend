9d9975855466eecd8542504da2586fcfe87ed0e8
Jaynayon <jay.nayonjr@cit.edu>
2024-04-22 03:42:00 +0800

    Add JEV entity

---
+++ models/jev.js
@@ -0,0 +1,81 @@
+const mongoose = require('mongoose');
+
+const JEVSchema = mongoose.Schema({
+    document: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Documents', //Documents model,
+        required: true //Needs to be under a Document 
+    },
+    respo_center: {
+        type: String,
+        required: false  // Assuming date is required 
+    },
+    account_explanation: {
+        type: String,
+        required: false  // Assuming particulars is required
+    },
+    uacs_obj_code: {
+        type: String,
+        required: false  // Assuming ors_burs_no is required
+    },
+    debit: {
+        type: Number,
+        required: false,
+        validate: [
+            {
+                validator: function (value) {
+                    // Check if 'value' is a number and >= 0
+                    return !isNaN(value) && typeof value === 'number' && value >= 0;
+                },
+                message: 'Amount must be a valid positive number'
+            },
+            {
+                validator: function (value) {
+                    // Check if 'value' is less than or equal to 10000
+                    return value <= 10000;
+                },
+                message: 'Amount must be less than or equal to 10000'
+            }
+        ],
+        default: 0, // Default value if not provided or invalid
+        set: function (value) {
+            // Automatically cap 'value' at 10000 if it exceeds this limit
+            return Math.min(value, 10000);
+        }
+    },
+    credit: {
+        type: Number,
+        required: false,
+        validate: [
+            {
+                validator: function (value) {
+                    // Check if 'value' is a number and >= 0
+                    return !isNaN(value) && typeof value === 'number' && value >= 0;
+                },
+                message: 'Amount must be a valid positive number'
+            },
+            {
+                validator: function (value) {
+                    // Check if 'value' is less than or equal to 10000
+                    return value <= 10000;
+                },
+                message: 'Amount must be less than or equal to 10000'
+            }
+        ],
+        default: 0, // Default value if not provided or invalid
+        set: function (value) {
+            // Automatically cap 'value' at 10000 if it exceeds this limit
+            return Math.min(value, 10000);
+        }
+    },
+    prepared_by: {
+        type: String,
+        required: false
+    },
+    certified_cor: {
+        type: String,
+        required: false
+    }
+});
+
+module.exports = mongoose.model('JEV', JEVSchema);
---
+++ routes/jevs.js
@@ -0,0 +1,177 @@
+const express = require('express')
+const mongoose = require('mongoose');
+const router = express.Router()
+const JEV = require('../models/jev')
+const Document = require('../models/document')
+
+//Get all JEVs
+router.get('/', async (req, res) => {
+    try {
+        const JEVs = await JEV.find();
+        res.json(JEVs)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
+
+//Creating one
+router.post('/', getDocument, async (req, res) => {
+    const { doc_id, respo_center, account_explanation, uacs_obj_code, debit, credit, prepared_by, certified_cor } = req.body;
+
+    // Validate required fields
+    if (!doc_id || !respo_center || !account_explanation || !uacs_obj_code || !prepared_by || !certified_cor ||
+        debit === undefined || debit === null || credit === undefined || credit === null
+    ) {
+        return res.status(400).json({ message: 'docId, espo_center, account_explanation, uacs_obj_code, debit, credit, prepared_by, and certified_cor are required' });
+    }
+
+    // Validate amount fields (debit and credit must be valid numbers)
+    if (isNaN(debit) || typeof debit !== 'number' || isNaN(credit) || typeof credit !== 'number') {
+        return res.status(400).json({ message: 'Debit and credit must be valid numbers' });
+    }
+
+    // Create a new LR document 
+    const jev = new JEV({
+        document: res.doc,
+        respo_center,
+        account_explanation,
+        uacs_obj_code,
+        debit,
+        credit,
+        prepared_by,
+        certified_cor
+    });
+    try {
+        //first save the lr to get the entire sum
+        const newJEV = await jev.save()
+
+        //calculate budget status
+        await calculateBudgetLimitStatus(res)
+
+        res.status(201).json(newJEV)
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
+})
+
+//Update one
+router.patch('/:id', getJEV, getDocumentByJev, async (req, res) => {
+    if (req.body.respo_center != null) {
+        res.jev.respo_center = req.body.respo_center
+    }
+    if (req.body.account_explanation != null) {
+        res.jev.account_explanation = req.body.account_explanation
+    }
+    if (req.body.uacs_obj_code != null) {
+        res.jev.uacs_obj_code = req.body.uacs_obj_code
+    }
+    if (req.body.debit != null) {
+        res.jev.debit = req.body.debit
+    }
+    if (req.body.credit != null) {
+        res.jev.credit = req.body.credit
+    }
+    if (req.body.prepared_by != null) {
+        res.jev.prepared_by = req.body.prepared_by
+    }
+    if (req.body.certified_cor != null) {
+        res.jev.certified_cor = req.body.certified_cor
+    }
+    try {
+        const newJev = await res.jev.save()
+
+        //calculate budget status
+        await calculateBudgetLimitStatus(res)
+
+        res.json(newJev)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
+
+//Deleting one
+router.delete('/:id', getJEV, getDocumentByJev, async (req, res) => {
+    try {
+        await res.jev.deleteOne()
+
+        //calculate budget status
+        await calculateBudgetLimitStatus(res)
+
+        res.json({ message: 'JEV removed' })
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
+
+
+//Middleware
+async function getJEV(req, res, next) {
+    let jev
+    try {
+        jev = await JEV.findById(req.params.id)
+        if (jev == null)
+            return res.status(404).json({ message: 'Cannot find LR' })
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
+    res.jev = jev
+    next()
+}
+
+async function getDocument(req, res, next) {
+    let doc
+    const docId = req.body.doc_id
+    if (!mongoose.isValidObjectId(docId)) {
+        return res.status(400).json({ message: 'Invalid Document ID format' });
+    }
+    try {
+        doc = await Document.findById(docId)
+        if (doc == null)
+            return res.status(404).json({ message: 'Cannot find Document' })
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
+    res.doc = doc
+    next()
+}
+
+async function getDocumentByJev(req, res, next) {
+    let doc
+    const docId = res.jev.document
+    if (!mongoose.isValidObjectId(docId)) {
+        return res.status(400).json({ message: 'Invalid Doc ID format' });
+    }
+    try {
+        doc = await Document.findById(docId)
+        if (doc == null)
+            return res.status(404).json({ message: 'Cannot find Document' })
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
+    res.doc = doc
+    next()
+}
+
+async function calculateBudgetLimitStatus(res) {
+    try {
+        const jevEntries = await JEV.find({ document: res.doc._id })
+            .select('-document') //we need the id to be used as keys
+
+        //get the sum on the amount property
+        const amountSum = jevEntries.reduce((total, jev) => total + jev.credit + jev.debit, 0);
+
+        // Check if the sum exceeds the budget_limit
+        if (amountSum > res.doc.budget_limit) {
+            res.doc.budget_exceeded = true;
+        } else {
+            res.doc.budget_exceeded = false;
+        }
+
+        const updatedDoc = await res.doc.save()
+    } catch (err) {
+        // Handle any errors
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
+
+module.exports = router
