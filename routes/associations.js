20d3043537ae3fcde0479ef14c30b2f5de5fc1b4
Jaynayon <jay.nayonjr@cit.edu>
2024-04-26 07:11:20 +0800

    Rename Position imports

--- routes/associations.js
+++ routes/associations.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose');
 const router = express.Router()
-const User = require('../models/user')
+const User = require('../models/User')
 const School = require('../models/school')
 const Association = require('../models/association');
 
--- routes/notifications.js
+++ routes/notifications.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose');
 const router = express.Router()
 const Notifications = require('../models/notification');
-const User = require("../models/user")
+const User = require("../models/User")
 const Association = require("../models/association")
 
 //Getting all
