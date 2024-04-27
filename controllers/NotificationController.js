5e681cf4a77cb26b707bb4af0c4c3a509302e6ee
Jaynayon <jay.nayonjr@cit.edu>
2024-04-28 04:26:36 +0800

    Add Notification Controller

---
+++ controllers/NotificationController.js
@@ -0,0 +1,179 @@
+const express = require('express')
+const mongoose = require('mongoose');
+const User = require('../models/User')
+const Notifications = require('../models/Notification')
+const Association = require("../models/Association")
+
+//Getting all
+async function getAllNotifications(req, res) {
+    try {
+        const notif = await Notifications.find()
+            .populate({
+                path: 'user',
+                select: '-_id -__v -password -position -username'
+            })
+            .select('-__v')
+        res.json(notif)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+}
+
+/*// Getting one position 
+async function getOnePosition(req, res) {
+    try {
+        console.log(res.pos)
+        res.send(res.pos)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+}*/
+
+// Creates new notification
+async function createNotification(req, res, next) {
+    //Check position name if it exists
+    const notif = new Notifications({
+        user: res.user,
+        assoc: res.assoc,
+        details: req.body.details
+    })
+    // Only include assoc if res.assoc is available (not null or undefined)
+    if (!res.assoc) {
+        delete notif.assoc;
+    }
+    try {
+        const newNotif = await notif.save()
+        res.status(201).json(newNotif)
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
+}
+
+//Patches notification
+async function patchNotification(req, res, next) {
+    if (req.body.details != null) {
+        res.notif.details = req.body.details
+    }
+    if (req.body.isRead != null) {
+        res.notif.isRead = req.body.isRead
+    }
+    try {
+        const newNotif = await res.notif.save()
+        res.json(newNotif)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+}
+
+async function deleteNotification(req, res) {
+    try {
+        await res.notif.deleteOne()
+        res.json({ message: 'Notification removed' })
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+}
+
+async function deleteAllNotificationByUser(req, res, next) {
+    const userId = req.params.user_id; // Assuming user_id is provided in the request body
+    try {
+        const result = await Notifications.deleteMany({
+            user: userId// Filter by userId
+        });
+
+        if (result.deletedCount > 0) {
+            res.json({ message: `${result.deletedCount} notification(s) removed` });
+        } else {
+            res.status(404).json({ message: 'No matching notifications found for deletion' });
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+}
+
+//Middleware
+async function getDuplicates(req, res, next) {
+    let notif
+    try {
+        notif = await Notifications.findOne({ name: req.body.name })
+        if (pos) {
+            return res.status(409).json({ message: 'Notification already exists' })
+        }
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
+    //Creates the object user
+    res.notif = notif;
+    next()//proceeds to the next function
+}
+
+async function getNotification(req, res, next) {
+    let notif
+    const notifId = req.params.id || req.body.id
+
+    if (!mongoose.isValidObjectId(notifId)) {
+        return res.status(400).json({ message: 'Invalid user ID format' });
+    }
+
+    try {
+        notif = await Notifications.findById(notifId)
+        if (notif == null)
+            return res.status(404).json({ message: 'Cannot find notification' })
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
+    res.notif = notif
+    next()
+}
+
+async function getUser(req, res, next) {
+    const userId = req.params.user_id || req.body.user_id
+
+    if (!mongoose.isValidObjectId(userId)) {
+        return res.status(400).json({ message: 'Invalid user ID format' });
+    }
+
+    try {
+        const user = await User.findById(userId);
+        if (user == null) {
+            return res.status(404).json({ message: 'Cannot find user' });
+        }
+        res.user = user; // Attach user object to response
+        next();
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+async function getAssociation(req, res, next) {
+    const assocId = req.params.assoc_id || req.body.assoc_id
+    if (!assocId) {
+        return next()
+    }
+    if (!mongoose.isValidObjectId(assocId)) {
+        return res.status(400).json({ message: 'Invalid assoc ID format' });
+    }
+
+    try {
+        const assoc = await Association.findById(assocId);
+        if (assoc == null) {
+            return res.status(404).json({ message: 'Cannot find Association' });
+        }
+        res.assoc = assoc; // Attach user object to response
+        next();
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+module.exports = {
+    getAllNotifications,
+    createNotification,
+    patchNotification,
+    deleteNotification,
+    deleteAllNotificationByUser,
+    getDuplicates,
+    getNotification,
+    getUser,
+    getAssociation,
+}
